69d44e70e2c76ec67e9345decac63570b19a0c7a
Peter Manoukian <thewizard_it@hotmail.com>
2026-05-05 22:08:22 +0400

    Adjust The Connetion strategy

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,23 +1,35 @@
 import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 
-// This loads the variables from your .env file
 dotenv.config();
 
+console.log("✅ DB_CA_CERT loaded:", !!process.env.DB_CA_CERT ? "YES" : "NO");
+console.log("CA Length:", process.env.DB_CA_CERT?.length || 0);
+
 const sequelize = new Sequelize(
-    process.env.DB_NAME as string, 
-    process.env.DB_USER as string, 
-    process.env.DB_PASS as string, 
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASS as string,
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
-        logging: false, // Set to true if you want to see the SQL commands in the terminal
+
+        logging: false,
+
         pool: {
             max: 5,
             min: 0,
             acquire: 30000,
-            idle: 10000
-        }
+            idle: 10000,
+        },
+
+        dialectOptions: {
+            ssl: {
+                ca: process.env.DB_CA_CERT?.replace(/\\n/g, '\n'),
+                rejectUnauthorized: true,
+                minVersion: 'TLSv1.2',
+            },
+        },
     }
 );
 
